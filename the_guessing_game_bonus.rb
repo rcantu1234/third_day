4d524861aceadce28fd61236b6f12266e2ac3d5c
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-07 14:55:47 -0600

    The Guessing Game Refactored - Bonus

--- the_guessing_game_bonus.rb
+++ the_guessing_game_bonus.rb
@@ -1,27 +1,39 @@
-mvputs "Welcome to the Guessing Game"
+class Play
+
+	attr_accessor :number_guesses
+	attr_accessor :users_choice
+	
+	def play
+	@number_guesses = 0
+	random_number = rand(100)
+	print "Pick a number between 0 - 100: "
+	@users_choice = gets.chomp
+
+		while @number_guesses < 5 do
+			puts "The computers number is #{random_number}."
+			print "Is it too high or too low? "
+			@users_choice = gets.chomp
+			if  @users_choice.downcase == "h" 
+				puts "Your guess is too high!  Please make another guess"
+				random_number = rand(0..random_number)
+				@number_guesses += 1
+			elsif users_choice.downcase == "l"
+				puts "Your guess is too low!  Please make another guess!"
+				random_number = rand(random_number..100)
+				@number_guesses += 1
+			elsif @users_choice == random_number
+				puts "You have guessed the number correctly after #{number_guesses + 1} guesses.  You win!"
+				
+			end
+		end
+			puts "I'm sorry, but you have exceeded the maximum guesses!"
+	end
+
+end
+
+puts "Welcome to the Guessing Game"
 puts "-" * 28
 
-number_guesses = 0
-random_number = rand(100)
-print "Pick a number between 0 - 100: "
-users_choice = gets.chomp
+game = Play.new
 
-while number_guesses < 5 do
-	puts "The computers number is #{random_number}."
-	print "Is it too high or too low? "
-	users_choice = gets.chomp
-	if  users_choice.downcase == "h" 
-		puts "Your guess is too high!  Please make another guess"
-		random_number = rand(0..random_number)
-		number_guesses += 1
-	elsif users_choice.downcase == "l"
-		puts "Your guess is too low!  Please make another guess!"
-		random_number = rand(random_number..100)
-		number_guesses += 1
-	elsif users_choice == random_number
-		puts "You have guessed the number correctly after #{number_guesses + 1} guesses.  You win!"
-		
-	end
-end
-	puts "I'm sorry, but you have exceeded the maximim guesses!"
-
+game.play
