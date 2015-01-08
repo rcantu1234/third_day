dfc61bffc1092b5243ca718ccc8a1295d447f110
ROEL CANTU <roel.cantu@yahoo.com>
2015-01-07 21:40:15 -0600

    Black Jack Refactored

--- black_jack.rb
+++ black_jack.rb
@@ -1,34 +1,49 @@
-class War
+class BlackJack
+	attr_accessor :player_one
+
 	def initialize
 		@player_one = Player.new
-		@player_two = Player.new
+		@card = Deck.new
 	end
 
 	def play
-		while @player_one.cards.count > 0 do 
+		puts "The player's balance is: #{player_one.balance}"
+		print "You must pay $10 to join the game.  Press Y to play or N to exit: "
+		users_choice = gets.chomp
+
+		player_one.balance -= 10
+		puts "Your balance is: #{player_one.balance}"
+
 			card_one = @player_one.cards.draw
-			card_two = @player_two.cards.draw
-			if card_one == card_two
-				puts "Tie - No One Wins!"
-			elsif card_one > card_two
-				@player_one.score += 1
-				puts "Player two scored this hand"
-			else
-				@player_two.score += 1
-				puts "Player two scored this hand"
-			end
-		end
-			puts "Final Score Is"
-			puts "--------------"
-			puts "Player One #{@player_one.score}"
-			puts "Player Two #{@player_two.score}"
+			puts "Your cards are: #{card_one}"
+
+			while @player_one.cards.count > 0 do 
+			print "Do you want to hit (h) or stay (s)? "
+			users_choice = gets.chomp
+
+			card = @card.hit
+
+				if users_choice == "h" 
+					puts "Your card is #{card}."
+					puts "Yours card's are: #{card_one.push(card)}"					
+				elsif users_choice == "s"
+					puts "Yours cards are: #{card_one}"
+					puts "The dealers cards are: #{@card.dealers_draw}"
+					break
+				end
+				
+			end			
+
+
+			#if users_choice == "h"
+		#end
 	end
 end
 
-
 class Player
-	attr_accessor :cards, :score
+	attr_accessor :cards, :score, :balance
 	def initialize
+		@balance = 100
 		@cards = Deck.new
 		@score = 0
 	end
@@ -36,9 +51,11 @@ end
 
 class Deck
 	attr_reader :cards
-
+	
 	def initialize
-		@cards = ((1..13).to_a * 4).shuffle
+		cards_that_equal_ten = [10] * 16
+		aces = [11] * 4
+		@cards = ((2..9).to_a * 4).concat(cards_that_equal_ten).concat(aces).shuffle
 	end
 
 	def count
@@ -49,10 +66,19 @@ class Deck
 		@cards = @cards.shuffle
 	end
 
+	def dealers_draw
+		@cards.shift(2)
+	end
+
 	def draw
+		@cards.shift(2)
+	end
+
+	def hit		
 		@cards.shift
 	end
 end
 
-game = War.new
+game = BlackJack.new
+
 game.play
